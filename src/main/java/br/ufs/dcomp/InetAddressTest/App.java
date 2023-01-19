eb66bf1c452c9171cd6c6a6d7bb4fed9c860b7f5
Ubuntu <ubuntu@ip-172-31-26-210.ec2.internal>
2023-01-19 20:07:54 +0000

    varios enderecos

--- src/main/java/br/ufs/dcomp/InetAddressTest/App.java
+++ src/main/java/br/ufs/dcomp/InetAddressTest/App.java
@@ -12,10 +12,16 @@ public class App
     {
         try{
             
-            String name = "www.ufs.br";
-            InetAddress address = InetAddress.getByName(name); 
+            String name = "www.google.com";
+            InetAddress address[] = InetAddress.getAllByName(name); 
+            
+            for(int i = 0; i < address.length; i++) {
+                 System.out.println( "Address:      "+ address[i].getHostAddress());
+            }
+            
+            
             System.out.println( "Name:      "+ name);
-            System.out.println( "Address:   "+ address.getHostAddress());
+            // System.out.println( "Address:   "+ address.getHostAddress());
 
         } catch (UnknownHostException e){
             System.out.println(e.getMessage());
